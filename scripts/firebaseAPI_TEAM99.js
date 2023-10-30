1ed7e13ce1b02238a18007035997735f5f9da7b8
lucaslav05 <lucaslaviolette05@gmail.com>
2023-10-29 22:16:02 -0700

    Added Login Page (Only Redirects Back to Index Since No main.html yet

---
+++ scripts/firebaseAPI_TEAM99.js
@@ -0,0 +1,20 @@
+//----------------------------------------
+//  Your web app's Firebase configuration
+//----------------------------------------
+var firebaseConfig = {
+
+    apiKey: "AIzaSyDWrZ1vtmlBxJVTdfV3ZuUarNhAttjRZcw",
+    authDomain: "recycleroute-34470.firebaseapp.com",
+    projectId: "recycleroute-34470",
+    storageBucket: "recycleroute-34470.appspot.com",
+    messagingSenderId: "867998236318",
+    appId: "1:867998236318:web:9edbf2adbeb5d5bda2458f",
+    measurementId: "G-RVR8EFQB19"
+};
+
+//--------------------------------------------
+// initialize the Firebase app
+// initialize Firestore database if using it
+//--------------------------------------------
+const app = firebase.initializeApp(firebaseConfig);
+const db = firebase.firestore();
