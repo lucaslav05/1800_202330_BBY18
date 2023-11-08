b82d653ebef7b6a38587297cc2c528923b100db8
jjhatu <j.jhatu1@gmail.com>
2023-11-08 11:54:34 -0800

    added link in main.html, created mybins.js, edited mybins.html

---
+++ scripts/mybins.js
@@ -0,0 +1,29 @@
+function displayMyPosts(collection) {
+    let cardTemplate = document.getElementById("my-posts-template")
+
+    db.collection(collection).where("status", "==", "Active")
+        .get()
+        .then(allBins=> {
+            allBins.forEach(doc => {
+                var title = doc.data().title;
+                var location = doc.data().location;
+                var type = doc.data().type;
+                var docID = doc.id;
+                
+
+                let newCard = cardTemplate.content.cloneNode(true);
+
+                newCard.querySelector("#my-post-title").innerHTML = title;
+                newCard.querySelector("#my-post-location").innerHTML = location;
+                newCard.querySelector("#my-post-item").innerHTML = type;
+                newCard.querySelector('#view-my-post-details').href = "mybinsinfo.html?docID="+docID;
+                document.getElementById("my-post-container").append(newCard);
+
+            })
+        })
+}
+
+displayMyPosts("posts");
+
+
+
