f70aa301b7ab1b6f4e1c5498b6ab624ca1ed8313
Dongharn Yi <dongharnyi32@gmail.com>
2023-11-23 09:03:42 -0800

    fixed error of bininfo.html

--- scripts/fav.js
+++ scripts/fav.js
@@ -1,82 +1,82 @@
 
 // const myArrayRemove = firebase.firestore.FieldValue.arrayRemove("east_coast")
-const auth = firebase.auth();
-let params = new URL(window.location.href);
-let binID = params.searchParams.get("docID");
-let userID;
-let userEmail;
+// const auth = firebase.auth();
+// let params = new URL(window.location.href);
+// let binID = params.searchParams.get("docID");
+// let userID;
+// let userEmail;
 
-auth.onAuthStateChanged(user => {
-    if (user) {
-        userID = user.uid;
-        userEmail = user.email;
-        const userDocRef = db.collection("users").doc(user.uid);
-        userDocRef
-            .get()
-            .then((doc) => {
-                if (doc.exists) {
-                    const userData = doc.data();
-                    try {
-                        if (userData && userData.favourites && userData.favourites.includes(binID)) {
-                            const favouritesWrap = document.getElementById("favouritesWrap");
-                            favouritesWrap.innerHTML = "<button type=\"button\" class=\"btn btn-primary btn-sm\" id=\"favouritesBtn\" onclick=\"removeFromFavorites()\">Remove from favourites</button>";
-                        } else {
-                            console.log("binID not found in user's favourites");
-                            favouritesWrap.innerHTML = "<button type=\"button\" class=\"btn btn-primary btn-sm\" id=\"favouritesBtn\" onclick=\"addToFavorites()\">Add to favourites</button>";
-                        }
-                    } catch (error) {
-                    }
+// auth.onAuthStateChanged(user => {
+//     if (user) {
+//         userID = user.uid;
+//         userEmail = user.email;
+//         const userDocRef = db.collection("users").doc(user.uid);
+//         userDocRef
+//             .get()
+//             .then((doc) => {
+//                 if (doc.exists) {
+//                     const userData = doc.data();
+//                     try {
+//                         if (userData && userData.favourites && userData.favourites.includes(binID)) {
+//                             const favouritesWrap = document.getElementById("favouritesWrap");
+//                             favouritesWrap.innerHTML = "<button type=\"button\" class=\"btn btn-primary btn-sm\" id=\"favouritesBtn\" onclick=\"removeFromFavorites()\">Remove from favourites</button>";
+//                         } else {
+//                             console.log("binID not found in user's favourites");
+//                             favouritesWrap.innerHTML = "<button type=\"button\" class=\"btn btn-primary btn-sm\" id=\"favouritesBtn\" onclick=\"addToFavorites()\">Add to favourites</button>";
+//                         }
+//                     } catch (error) {
+//                     }
                     
-                } else {
-                    console.log("User document not found");
-                }
-            })
-            .catch((error) => {
-                console.error("Error getting user document:", error);
-            });
-    } else {
-        window.location.assign("login.html");
-    }
-});
+//                 } else {
+//                     console.log("User document not found");
+//                 }
+//             })
+//             .catch((error) => {
+//                 console.error("Error getting user document:", error);
+//             });
+//     } else {
+//         window.location.assign("login.html");
+//     }
+// });
 
-function addToFavorites() {
-    const favouritesUnion = firebase.firestore.FieldValue.arrayUnion(binID)
-    db.collection("users").doc(userID).update({
-        favourites: favouritesUnion
-    }).then(function () {
-        console.log("New favorite bin to user document in firestore");
-    }).catch(function (error) {
-        console.log("Error adding new user: " + error);
-    });
-    const favedByUserUnion = firebase.firestore.FieldValue.arrayUnion(userID)
-    db.collection("posts").doc(binID).update({
-        favedByUser: favedByUserUnion
-    }).then(function () {
-        console.log("New user added to bin document firestore");
-        window.location.assign("favourites.html");
-    }).catch(function (error) {
-        console.log("Error adding new user: " + error);
-    });
-}
-function removeFromFavorites() {
-    const favouritesUnion = firebase.firestore.FieldValue.arrayRemove(binID)
-    db.collection("users").doc(userID).update({
-        favourites: favouritesUnion
-    }).then(function () {
-        console.log("New favorite bin to user document in firestore");
-    }).catch(function (error) {
-        console.log("Error adding new user: " + error);
-    });
-    const favedByUserUnion = firebase.firestore.FieldValue.arrayRemove(userEmail + " ID: " + userID)
-    db.collection("posts").doc(binID).update({
-        favedByUser: favedByUserUnion
-    }).then(function () {
-        console.log("New user added to bin document firestore");
-        window.location.assign("favourites.html");
-    }).catch(function (error) {
-        console.log("Error adding new user: " + error);
-    });
-}
+// function addToFavorites() {
+//     const favouritesUnion = firebase.firestore.FieldValue.arrayUnion(binID)
+//     db.collection("users").doc(userID).update({
+//         favourites: favouritesUnion
+//     }).then(function () {
+//         console.log("New favorite bin to user document in firestore");
+//     }).catch(function (error) {
+//         console.log("Error adding new user: " + error);
+//     });
+//     const favedByUserUnion = firebase.firestore.FieldValue.arrayUnion(userID)
+//     db.collection("posts").doc(binID).update({
+//         favedByUser: favedByUserUnion
+//     }).then(function () {
+//         console.log("New user added to bin document firestore");
+//         window.location.assign("favourites.html");
+//     }).catch(function (error) {
+//         console.log("Error adding new user: " + error);
+//     });
+// }
+// function removeFromFavorites() {
+//     const favouritesUnion = firebase.firestore.FieldValue.arrayRemove(binID)
+//     db.collection("users").doc(userID).update({
+//         favourites: favouritesUnion
+//     }).then(function () {
+//         console.log("New favorite bin to user document in firestore");
+//     }).catch(function (error) {
+//         console.log("Error adding new user: " + error);
+//     });
+//     const favedByUserUnion = firebase.firestore.FieldValue.arrayRemove(userEmail + " ID: " + userID)
+//     db.collection("posts").doc(binID).update({
+//         favedByUser: favedByUserUnion
+//     }).then(function () {
+//         console.log("New user added to bin document firestore");
+//         window.location.assign("favourites.html");
+//     }).catch(function (error) {
+//         console.log("Error adding new user: " + error);
+//     });
+// }
 
 
 
