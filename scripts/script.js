d4ef05b084c203d03a8618519abf3485e5aeac7f
Olivia Grace <ograce2@my.bcit.ca>
2023-12-01 20:11:35 -0800

    Organized the code for map.html, mybins.html, newpost.html, thankyou.html, script.js, skeleton.js, and username.js. Adjusted formatting of newpost.html

--- scripts/script.js
+++ scripts/script.js
@@ -1,11 +1,11 @@
 //------------------------------------------------
-// Call this function when the "logout" button is clicked
+// This function is called on login.html and index.html to ensure users are logged out.
 //-------------------------------------------------
 function logout() {
-    firebase.auth().signOut().then(() => {
-        // Sign-out successful.
-        console.log("logging out user");
-      }).catch((error) => {
-        // An error happened.
-      });
+  firebase.auth().signOut().then(() => {
+    // Sign-out successful.
+    console.log("logging out user");
+  }).catch((error) => {
+    // An error happened.
+  });
 }
--- scripts/username.js
+++ scripts/username.js
@@ -1,20 +1,20 @@
+
+// This function is used to display the user's name on pages to create a personalized experience
+// for the user.
 function getNameFromAuth() {
     firebase.auth().onAuthStateChanged(user => {
         // Check if a user is signed in:
         if (user) {
-            // Do something for the currently logged-in user here: 
-            console.log(user.uid); //print the uid in the browser console
-            console.log(user.displayName);  //print the user name in the browser console
+
+            //print the uid and user name in the browser console
+            console.log(user.uid);
+            console.log(user.displayName);
+
+            // Get the user's display name
             userName = user.displayName;
-            document.getElementById("name-goes-here").innerText = userName;    
-            //method #1:  insert with JS
-            //document.getElementById("name-goes-here").innerText = userName;    
 
-            //method #2:  insert using jquery
-            //$("#name-goes-here").text(userName); //using jquery
-
-            //method #3:  insert using querySelector
-            //document.querySelector("#name-goes-here").innerText = userName
+            // Put the user's display name on the page
+            document.getElementById("name-goes-here").innerText = userName;
 
         } else {
             // No user is signed in.
