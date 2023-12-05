0aa55a2d0d0ab5b057dc1945123a761a669fb8ab
jjhatu <j.jhatu1@gmail.com>
2023-12-05 11:41:45 -0800

    oragnized code in binifo.js, mybins.js, viewmyposts.js, and bininfo.html

--- scripts/mybins.js
+++ scripts/mybins.js
@@ -1,30 +1,3 @@
-// function displayMyPosts(collection) {
-//     let cardTemplate = document.getElementById("my-posts-template")
-
-//     db.collection(collection).where("location, "==", "1234 Bcit Lane")
-//         .get()
-//         .then(allBins=> {
-//             allBins.forEach(doc => {
-//                 var title = doc.data().title;
-//                 var location = doc.data().location;
-//                 var type = doc.data().type;
-//                 var docID = doc.id;
-                
-
-//                 let newCard = cardTemplate.content.cloneNode(true);
-
-//                 newCard.querySelector("#my-post-title").innerHTML = title;
-//                 newCard.querySelector("#my-post-location").innerHTML = location;
-//                 newCard.querySelector("#my-post-item").innerHTML = type;
-//                 newCard.querySelector('#view-my-post-details').href = "mybinsinfo.html?docID="+docID;
-//                 document.getElementById("my-post-container").append(newCard);
-
-//             })
-//         })
-// }
-
-// displayMyPosts("posts");
-
 function getMyPosts() {
 
     
